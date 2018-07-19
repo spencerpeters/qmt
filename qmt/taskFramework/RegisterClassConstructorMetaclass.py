34a619639d0c55ef366f05f965dd19e366ba9fea
Spencer Peters <t-sppete@microsoft.com>
2018-07-19 14:54:31 -0700

    added registration metaclass

--- qmt/taskFramework/RegisterClassConstructorMetaclass.py
+++ qmt/taskFramework/RegisterClassConstructorMetaclass.py
@@ -3,3 +3,4 @@ class RegisterClassConstructorMetaclass(type):
     def __new__(mcs, name, bases, class_dict):
         cls = type.__new__(mcs, name, bases, class_dict)
         print("Metaclass!")
+        return cls
