06982dc342c5b984b4a27fafc44d54d97605c632
Spencer Peters <t-sppete@microsoft.com>
2018-07-19 16:09:33 -0700

    removed properties

--- qmt/taskFramework/IncrementTask.py
+++ qmt/taskFramework/IncrementTask.py
@@ -8,15 +8,15 @@ class IncrementTask(Task):
         super(IncrementTask, self).__init__(state=state, name=name)
         self._number = number
 
-    @property
-    def number(self):
-        return self._number
-
-    @number.setter
-    def number(self, number):
-        if number < 0:
-            raise ValueError("number must be >= 0")
-        self._number = number
+    # @property
+    # def number(self):
+    #     return self._number
+    #
+    # @number.setter
+    # def number(self, number):
+    #     if number < 0:
+    #         raise ValueError("number must be >= 0")
+    #     self._number = number
 
     @staticmethod
     def from_serialized_form(name, state, dependencies):
