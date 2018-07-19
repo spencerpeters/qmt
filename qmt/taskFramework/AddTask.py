608c163971f4e02870d65535ed191a5666986147
Spencer Peters <t-sppete@microsoft.com>
2018-07-19 11:28:55 -0700

    bug

--- qmt/taskFramework/AddTask.py
+++ qmt/taskFramework/AddTask.py
@@ -3,6 +3,7 @@ import json
 from IncrementTask import IncrementTask
 from Task import Task
 
+
 def main():
     leftIncrementTask = IncrementTask(0)
     rightIncrementTask = IncrementTask(1)
@@ -12,25 +13,22 @@ def main():
     print(addTask.result)
     print(addTask.compute())
 
+
 class AddTask(Task):
 
     def __init__(self, leftIncrementTask, rightIncrementTask, number, name="AddTask"):
-        state = {'number' : number}
+        state = {'number': number}
         super(AddTask, self).__init__(state=state, dependencies=[leftIncrementTask, rightIncrementTask], name=name)
         self.leftIncrementTask = leftIncrementTask
         self.rightIncrementTask = rightIncrementTask
         self.number = number
 
     def run(self):
-        if self.result is not None:
-            return
-
         x = delayed(self.leftIncrementTask.run)()
         y = delayed(self.rightIncrementTask.run)()
         delayedAdd = delayed(sum([x, y, self.number]))
         return delayedAdd
 
 
-
-if __name__=="main":
+if __name__ == "main":
     main()
