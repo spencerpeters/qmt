edd40d9e7e88d8e3036d9a4597bced3b5c73f41d
Spencer Peters <t-sppete@microsoft.com>
2018-07-19 16:10:11 -0700

    removed properties

--- qmt/taskFramework/IncrementTask.py
+++ qmt/taskFramework/IncrementTask.py
@@ -6,7 +6,7 @@ class IncrementTask(Task):
     def __init__(self, number, name="IncrementTask"):
         state = {'number': number}
         super(IncrementTask, self).__init__(state=state, name=name)
-        self._number = number
+        self.number = number
 
     # @property
     # def number(self):
